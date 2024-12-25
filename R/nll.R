7924d89d3a07fd112270329afa3729a81ae6c5fb
Ethan Lawler <lawlerem@gmail.com>
2024-12-25 02:24:52 -0400

    Linted code and updated documentation

--- R/nll.R
+++ R/nll.R
@@ -4,20 +4,20 @@ nll<- function(pars) {
     RTMB::getAll(pars)
     ll<- 0
 
-    spline_parameters<- exp(working_spline_parameters)
+    spline_parameters<- working_spline_parameters |> exp()
 
-    rate_spline<- nnspline::update_spline(
-        spline,
-        parameters = spline_parameters[, 1],
-        node_values = node_values[, 1]
-    )
+    rate_spline<- spline |>
+        nnspline::update_spline(
+            parameters = spline_parameters[, 1],
+            node_values = node_values[, 1]
+        )
     ll<- ll + nnspline::dspline(node_values[, 1], rate_spline, log = TRUE)
 
-    max_length_spline<- nnspline::update_spline(
-        spline,
-        parameters = spline_parameters[, 2],
-        node_values = node_values[, 2]
-    )
+    max_length_spline<- spline |> 
+        nnspline::update_spline(
+            parameters = spline_parameters[, 2],
+            node_values = node_values[, 2]
+        )
     ll<- ll + nnspline::dspline(node_values[, 2], max_length_spline, log = TRUE)
 
     shift<- -exp(working_shift)
@@ -27,25 +27,19 @@ nll<- function(pars) {
         exp(mu_length + max_length_spline$values[data$spline_idx]),
         shift
     )
-    # raw_ll<- RTMB::dlnorm(
-    #     x = log(data$length),
-    #     meanlog = log(data$prediction),
-    #     sdlog = exp(working_sd),
-    #     log = TRUE
-    # )
-    raw_ll<- RTMB::dnorm(
-        data$length,
-        data$prediction,
-        exp(working_sd) * sqrt(data$prediction),
+    raw_ll<- RTMB::dlnorm(
+        x = data$length,
+        meanlog = log(data$prediction),
+        sdlog = exp(working_sd),
         log = TRUE
     )
     ll<- ll + sum(robustifyRTMB::robustify(raw_ll, robustness, "ll"))
-    weights<- robustifyRTMB::robust_weight(raw_ll, robustness, "ll")
+    weights<- raw_ll |> robustifyRTMB::robust_weight(robustness, "ll")
 
     fixed_parameters<- c(mu_rate, mu_length, shift)
-    mcreportRTMB::MCREPORT(spline_parameters)
-    mcreportRTMB::MCREPORT(node_values)
-    mcreportRTMB::MCREPORT(fixed_parameters)
-    mcreportRTMB::MCREPORT(weights)
+    spline_parameters |> mcreportRTMB::MCREPORT()
+    node_values |> mcreportRTMB::MCREPORT()
+    fixed_parameters |> mcreportRTMB::MCREPORT()
+    weights |> RTMB::REPORT()
     return( -ll )
 }
--- R/stvonb.R
+++ R/stvonb.R
@@ -1,10 +1,26 @@
 #' Fit a von Bertalanffy growth curve with spatially  varying parameters.
 #' 
-#' @param data An sf data.frame with point geometries, an age column, and a length column.
-#' @param nodes Either an sf data.frame giving the point location of nodes or an integer giving the number of nodes which will be smartly chosen.
-#' @param ... Additional arguments, particularly those to pass to nnspline::create_nnspline, robustifyRTMB::robustly_optimize, and mcreportRTMB::mcreport.
+#' @param data 
+#'     An sf data.frame with point geometries, an age column, and a length 
+#'     column.
+#' @param nodes 
+#'     Either an sf data.frame giving the point location of nodes or an integer 
+#'     giving the number of nodes which will be smartly chosen.
+#' @param ... Additional arguments, particularly those to pass to 
+#'     nnspline::create_nnspline, robustifyRTMB::robustly_optimize, and 
+#'     mcreportRTMB::mcreport.
 #'   
-#' @return A list containing the model fit, a template spline, and mcreported spline realizations
+#' @return A list containing:
+#'   * robust_optimization
+#'      The output of robustifyRTMB::robustly_optimize
+#'   * template_spline
+#'       An nnspline objet used to hold the nodes and node graph used for
+#'       the model
+#'   * mcreport
+#'       The output of mcreportRTMB::mcreport for the spline parameters,
+#'       node values, and mean rate, max length, and length zero age.
+#'   * call
+#'       The function call used.
 #' 
 #' @export
 fit_stvonb<- function(
@@ -14,27 +30,30 @@ fit_stvonb<- function(
     ) {
     call<- match.call(expand.dots = TRUE) |> as.list()
 
-    if( "sf" %in% class(nodes) ) {
-        nodes<- unique(sf::st_coordinates(nodes))
-    } else if( is.numeric(nodes) ) {
+    if( "sf" %in% (nodes |> class()) ) {
+        nodes<- nodes |> sf::st_coordinates() |> unique()
+    } else if( nodes |> is.numeric() ) {
         nodes<- nnspline::nnodes(
-            sf::st_coordinates(data),
-            n_nodes = max(1, floor(nodes)),
+            data |> sf::st_coordinates(),
+            n_nodes = max(1, nodes |> floor()),
             max.it = 200
         )$nodes
     } else { stop("Nodes must be given as an sf data.frame or as an integer.") }
-    spline_args<- call[names(call) %in% names(formals(nnspline::create_nnspline))]
-    spline_args$x<- unique(sf::st_coordinates(data))
+    arg_names<- nnspline::create_nnspline |> formals() |> names()
+    spline_args<- call[(call |> names()) %in% arg_names]
+    spline_args$x<- data |> sf::st_coordinates() |> unique()
     spline_args$nodes<- nodes
-    spline<- do.call(nnspline::create_nnspline, spline_args)
-    data$spline_idx<- nnspline::nns(
-        sf::st_coordinates(data),
-        spline,
-        index = TRUE
-    )
+    spline<- nnspline::create_nnspline |> do.call(spline_args)
+    data$spline_idx<- data |> 
+        sf::st_coordinates() |>
+        nnspline::nns(
+            spline,
+            index = TRUE
+        )
     
     # Fit a model to the data
-    robopt_args<- call[names(call) %in% names(formals(robustifyRTMB::robustly_optimize))]
+    arg_names<- robustifyRTMB::robustly_optimize |> formals() |> names()
+    robopt_args<- call[(call |> names()) %in% arg_names]
     environment(nll)<- environment()
     robopt_args$func<- nll
     robopt_args$parameters<- list(
@@ -51,21 +70,22 @@ fit_stvonb<- function(
         mu_rate = 0,
         mu_length = with(
             data,
-            log(mean(length[age > quantile(age, 0.8)]))
+            length[age > quantile(age, 0.8)] |> mean() |> log()
         ),
         working_shift = 0,
         working_sd = 0
     )
-    robopt_args$random<- c("node_values")
-    robopt_args$smooth<- c("node_values")
+    robopt_args$random<- "node_values"
+    robopt_args$smooth<- "node_values"
     robopt_args$nodes<- spline$nodes
-    fit<- do.call(robustifyRTMB::robustly_optimize, robopt_args)
+    fit<- robustifyRTMB::robustly_optimize |> do.call(robopt_args)
 
     # Get mcreports of spline parameters and node values
-    mcreport_args<- call[names(call) %in% names(formals(mcreportRTMB::mcreport))]
+    arg_names<- mcreportRTMB::mcreport |> formals() |> names()
+    mcreport_args<- call[(call |> names()) %in% arg_names]
     mcreport_args$obj<- fit$obj
     mcreport_args$sdr<- fit$sdr
-    mc<- do.call(mcreportRTMB::mcreport, mcreport_args)
+    mc<- mcreportRTMB::mcreport |> do.call(mcreport_args)
 
     ans<- list(
         fit = fit,
@@ -78,12 +98,17 @@ fit_stvonb<- function(
 
 #' Predict spatial growth curves
 #' 
-#' @param fit A fitted model from fit_stvonb
-#' @param raster A stars raster
-#' @param age Ages to use for length-at-age predictions, if any.
-#' @param quantiles The quantiles to predict. Defaults to a 95% confidence interval around the mean
+#' @param fit 
+#'     A fitted model from fit_stvonb
+#' @param raster 
+#'     A stars raster
+#' @param age 
+#'     Ages to use for length-at-age predictions, if any.
+#' @param quantiles 
+#'     The quantiles to predict. Defaults to c(0.05, 0.5, 0.95).
 #' 
-#' @return A list with the spatial von Bertalanffy parameter predictions and spatial length-at-age predictions
+#' @return A list with the spatial von Bertalanffy parameter predictions and 
+#'     spatial length-at-age predictions
 #' 
 #' @export
 predict_stvonb<- function(
@@ -106,7 +131,7 @@ predict_stvonb<- function(
 
     # 1.) recreate spline with the same nodes but x is updated to raster locations
     spline<- nnspline::create_nnspline(
-        x = sf::st_coordinates(raster) |> as.matrix(),
+        x = raster |> sf::st_coordinates() |> as.matrix(),
         nodes = fit$template_spline$nodes,
         n_parents = fit$template_spline$n_parents,
         parameters = fit$template_spline$parameters,
@@ -119,119 +144,125 @@ predict_stvonb<- function(
     mc<- fit$mcreport
     vonb_predictions<- c(raster[1], raster[1], along = "variable")
     names(vonb_predictions)<- "value"
-    vonb_predictions<- stars::st_set_dimensions(
-        vonb_predictions,
-        "variable",
-        values = c("rate", "max_length")
-    )
+    vonb_predictions<- vonb_predictions |>
+        stars::st_set_dimensions(
+            "variable",
+            values = c("rate", "max_length")
+        )
 
-    mcreport_args<- fit$call[names(fit$call) %in% names(formals(mcreportRTMB::mcreport))]
-    parallel<- if( "parallel" %in% names(mcreport_args) ) mcreport_args$parallel else 1
-    trace<- if( "trace" %in% names(mcreport_args) ) mcreport_args$trace else TRUE
+
+    arg_names<- mcreportRTMB::mcreport |> formals() |> names()
+    mcreport_args<- fit$call[(fit$call |> names()) %in% arg_names]
+    parallel<- if( "parallel" %in% (mcreport_args |> names()) ) {
+        mcreport_args$parallel
+     } else {
+        1
+     }
+    silent<- if( "silent" %in% (mcreport_args |> names()) ) {
+        mcreport_args$silent 
+    } else {
+        TRUE
+    }
     if( (parallel > 1) && requireNamespace("parallel", quietly = TRUE) ) {
         lapplyfn<- parallel::mclapply
     } else {
         lapplyfn<- lapply
     }
-    vonb_predictions<- lapplyfn(
-        seq(ncol(mc[[1]])),
-        function(i, ...) {
-            if( trace ) {
-                cat(
-                    paste0(
-                        "(",
-                        i,
-                        " / ",
-                        ncol(mc[[1]]),
-                        ")\n"
-                    )
+    interpolate_replicate<- function(i, ...) {
+        if( !silent ) {
+            cat(
+                paste0(
+                    "\rUpdating mcreplicate: (", i, " / ", 
+                    mc[[1]] |> ncol(), ")"
                 )
-            }
-            rate_spline<- nnspline::update_spline(
-                spline,
-                parameters = mc$spline_parameters[, 1, i],
-                node_values = mc$node_values[, 1, i]
             )
-            max_length_spline<- nnspline::update_spline(
-                spline,
-                parameters = mc$spline_parameters[, 2, i],
-                node_values = mc$node_values[, 2, i]
-            )
-            vonb_predictions$value[, , 1]<- exp(mc$fixed_parameters[1, i] + rate_spline$values)
-            vonb_predictions$value[, , 2]<- exp(mc$fixed_parameters[2, i] + max_length_spline$values)
-            return(vonb_predictions)
-        },
-        mc.cores = parallel,
-        mc.preschedule = FALSE
-    )
-    vonb_predictions<- do.call(
-        c,
-        c(
-            vonb_predictions,
-            list(along = "replicate")
+            if( i == (mc[[1]] |> ncol()) ) cat("\n")
+            flush.console()
+        }
+        rate_spline<- nnspline::update_spline(
+            spline,
+            parameters = mc$spline_parameters[, 1, i],
+            node_values = mc$node_values[, 1, i]
         )
-    )
+        max_length_spline<- nnspline::update_spline(
+            spline,
+            parameters = mc$spline_parameters[, 2, i],
+            node_values = mc$node_values[, 2, i]
+        )
+        vonb_predictions$value[, , 1]<- exp(mc$fixed_parameters[1, i] + rate_spline$values)
+        vonb_predictions$value[, , 2]<- exp(mc$fixed_parameters[2, i] + max_length_spline$values)
+        return(vonb_predictions)
+    }
+    vonb_predictions<- mc[[1]] |> 
+        ncol() |> 
+        seq() |>
+        lapplyfn(
+            interpolate_replicate,
+            mc.cores = parallel,
+            mc.preschedule = FALSE
+        )
+    vonb_predictions<- c |> 
+        do.call(
+            c(
+                vonb_predictions,
+                list(along = "replicate")
+            )
+        )
 
-    vonb_quantiles<- stars::st_apply(
-        vonb_predictions,
-        MARGIN = 1:3,
-        quantile,
-        probs = quantiles,
-        na.rm = TRUE
-    )
-    vonb_quantiles<- aperm(
-        vonb_quantiles,
-        c(2:4, 1)
-    )
+    vonb_quantiles<- vonb_predictions |>
+        stars::st_apply(
+            MARGIN = 1:3,
+            quantile,
+            probs = quantiles,
+            na.rm = TRUE
+        )
+    vonb_quantiles<- vonb_quantiles |> aperm(c(2:4, 1))
     
     ans$parameters<- vonb_quantiles
-    if( missing(age) ) return(ans)
+    if( missing(age) ) return( ans )
 
     # 3.) Predict the length at age for each replicate
-    length_predictions<- lapply(
-        age,
-        function(a) {
-            age_prediction<- vonb_predictions[, , , 1, ]
-            age_prediction<- stars::st_set_dimensions(
-                age_prediction,
-                "variable",
-                names = "age",
-                values = as.character(a)
-            )
-            age_prediction$value[, , 1, ]<- vonb(
-                age = a,
-                rate = vonb_predictions$value[, , 1, ],
-                max_length = vonb_predictions$value[, , 2, ],
-                shift = array(
-                    rep(
-                        mc$fixed_parameters[3, ],
-                        each = prod(dim(vonb_predictions)[1:2])
-                    ),
-                    dim = dim(vonb_predictions$value[, , 1, ])
+    length_predictions<- age |>
+        lapply(
+            function(a) {
+                age_prediction<- vonb_predictions[, , , 1, ]
+                age_prediction<- age_prediction |>
+                    stars::st_set_dimensions(
+                        "variable",
+                        names = "age",
+                        values = a |> as.character()
+                    )
+                age_prediction$value[, , 1, ]<- vonb(
+                    age = a,
+                    rate = vonb_predictions$value[, , 1, ],
+                    max_length = vonb_predictions$value[, , 2, ],
+                    shift = array(
+                        rep(
+                            mc$fixed_parameters[3, ],
+                            each = prod(dim(vonb_predictions)[1:2])
+                        ),
+                        dim = dim(vonb_predictions$value[, , 1, ])
+                    )
                 )
-            )
-            return(age_prediction)
-        }
-    )
-    length_predictions<- do.call(
-        c,
-        c(
-            length_predictions,
-            list(along = "age")
+                return(age_prediction)
+            }
         )
-    )
-    length_quantiles<- stars::st_apply(
-        length_predictions,
-        MARGIN = 1:3,
-        quantile,
-        probs = quantiles,
-        na.rm = TRUE
-    )
-    length_quantiles<- aperm(
-        length_quantiles,
-        c(2:4, 1)
-    )
+    length_predictions<- c |>
+        do.call(
+            c(
+                length_predictions,
+                list(along = "age")
+            )
+        )
+    length_quantiles<- length_predictions |>
+        stars::st_apply(
+            MARGIN = 1:3,
+            quantile,
+            probs = quantiles,
+            na.rm = TRUE
+        )
+    length_quantiles<- length_quantiles |> aperm(c(2:4, 1))
     ans$length_at_age<- length_quantiles
 
-    return(ans)
+    return( ans )
 }
